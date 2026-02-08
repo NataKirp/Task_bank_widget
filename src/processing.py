ddd339cb3892e3d1ff714e56ddf5d49e3fb6d676
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-02-09 00:50:58 +0300

    add processing.py with function filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,24 @@
+def filter_by_state(list_of_dicts: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """
+    Функция принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
+    Функция возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению.
+    :param list_of_dicts: Список словарей
+    :param state: Ключ, по значению которого фильтруется список (по умолчанию 'EXECUTED')
+    :return: Список с заданным значением ключа
+    """
+    target_state = []
+    for item in list_of_dicts:
+        if item["state"] == state:
+            target_state.append(item)
+    return target_state
+
+
+if __name__ == "__main__":
+    transactions = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+    print(filter_by_state(transactions, "EXECUTED"))
