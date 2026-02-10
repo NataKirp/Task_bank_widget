985cd0ce3633f066871c3e66e19efc3b3b080d0d
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-02-10 14:58:12 +0300

    add fixtures for func filter_by_date in conftest.py add tests for func filter_by_date in test_processing.py, modified func filter_by_date in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 def filter_by_state(list_of_dicts: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Функция принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
@@ -23,16 +26,18 @@ def sort_by_date(list_of_dicts: list[dict], descending: bool = True) -> list[dic
     :param descending: Порядок сортировки (False - возрастание, True - убывание)
     :return: Отсортированный список
     """
-    return sorted(list_of_dicts, key=lambda item: item["date"], reverse=descending)
+
+    return sorted(list_of_dicts, key=lambda item: (datetime.datetime.fromisoformat(item["date"]), item["id"]),
+                  reverse=descending)
 
 
-if __name__ == "__main__":
-    transactions = [
-        {"id": 41428829, "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "date": "2018-10-14T08:21:33.419441"},
-    ]
-    print(filter_by_state(transactions, "EXECUTED"))
-    print(filter_by_state(transactions))
-    print(sort_by_date(transactions))
+# if __name__ == "__main__":
+# transactions = [
+#     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+#     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+#     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+# ]
+# print(filter_by_state(transactions, "EXECUTED"))
+# print(filter_by_state(transactions))
+# print(sort_by_date(transactions))
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
 from tests.conftest import transactions
 
 
@@ -17,3 +17,25 @@ def test_filter_by_state(transactions, transactions_state_executed, transactions
 # Тестирование работы функции при отсутствии словарей со статусом state
 def test_filter_by_state_no_state(transactions_without_state):
     assert filter_by_state(transactions_without_state) == []
+
+
+# Тестирование сортировки списка словарей по датам в порядке убывания, возрастания и по умолчанию.
+@pytest.mark.parametrize("descending", ["False",
+                                        "True",
+                                        ""])
+def test_sort_by_date(transactions, descending, sort_by_date_descending, sort_by_date_ascending):
+    assert sort_by_date(transactions, descending=True) == sort_by_date_descending
+    assert sort_by_date(transactions, descending=False) == sort_by_date_ascending
+    assert sort_by_date(transactions) == sort_by_date_descending
+
+
+# Тестирование корректности сортировки при одинаковых датах
+def test_sort_by_date_same_date(transactions_same_date):
+    assert transactions_same_date == transactions_same_date
+
+
+# Тестирование работы функции с некорректными или нестандартными форматами дат
+def test_sort_by_date_wrong_date(transactions_wrong_date):
+    with pytest.raises(ValueError):
+        sort_by_date(transactions_wrong_date)
+
