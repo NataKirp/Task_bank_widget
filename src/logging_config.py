5c1ea546062f71ccc4a9d54ec34a4ee58de8061a
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-03-12 00:53:24 +0300

    add logging_config.py with setup_logging func

---
+++ src/logging_config.py
@@ -0,0 +1,11 @@
+import logging
+
+
+def setup_logging(module_name: str):
+    logger = logging.getLogger(module_name)
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(f'../logs/{module_name}.log', mode='w', encoding='utf-8')
+    file_formatter = logging.Formatter('%(asctime)s : %(filename)s : %(levelname)s : %(message)s')
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+    return logger
