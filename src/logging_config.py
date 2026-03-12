bd7df1f4ab8b9040c1b8bd6857ee52a8197c96cb
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-03-12 22:07:44 +0300

    fixes in logging_config.py and README.md

--- src/logging_config.py
+++ src/logging_config.py
@@ -1,10 +1,13 @@
 import logging
+import os
+
+os.chdir('C:/Users/Vasilyev/Documents/Python/Homework/task_bank_widget')
 
 
 def setup_logging(module_name: str):
     logger = logging.getLogger(module_name)
     logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler(f'../logs/{module_name}.log', mode='w', encoding='utf-8')
+    file_handler = logging.FileHandler(f'logs/{module_name}.log', mode='w', encoding='utf-8')
     file_formatter = logging.Formatter('%(asctime)s : %(filename)s : %(levelname)s : %(message)s')
     file_handler.setFormatter(file_formatter)
     logger.addHandler(file_handler)
