6bab75b4af9b5809a0eaee0a144c6868a71791f9
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-04-03 00:52:18 +0300

    small fixes in function mask_account_card and test for it

--- src/widget.py
+++ src/widget.py
@@ -3,21 +3,20 @@ import re
 
 from src.masks import get_mask_account, get_mask_card_number
 
+CARD_NUMBER_LENGTH = 16
+
 
 def mask_account_card(bank_details: str) -> str:
     """Функция принимает тип и номер карты или счета и возвращает замаскированный номер"""
-    bank_details_type = "".join(re.findall(r"\D+", bank_details)).rstrip()
+    if bank_details == '':
+        return 'нет данных'
+
+    bank_details_type = "".join(re.findall(r"[a-zA-Z]+\s|^МИР|^Счет", bank_details)).rstrip()
     bank_details_number = "".join(re.findall(r"\d", bank_details))
     if bank_details.startswith("Счет"):
         masked_number = f"{bank_details_type} {get_mask_account(bank_details_number)}"
-    elif (
-        bank_details_type.startswith("Master")
-        or bank_details_type.startswith("Visa")
-        or bank_details_type.startswith("Maestro")
-    ):
-        masked_number = (
-            f"{bank_details_type} {get_mask_card_number(bank_details_number)}"
-        )
+    elif len(bank_details_number) == CARD_NUMBER_LENGTH:
+        masked_number = f"{bank_details_type} {get_mask_card_number(bank_details_number)}"
     else:
         raise ValueError("Неверное обозначение карты или счета")
     return masked_number
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -7,20 +7,20 @@ from src.widget import get_date, mask_account_card
 def test_mask_account_card():
     assert mask_account_card("Счет 73654108430135874305") == "Счет **4305"
     assert (
-        mask_account_card("Visa Platinum 7000792289606361")
-        == "Visa Platinum 7000 79** **** 6361"
+            mask_account_card("Visa Platinum 7000792289606361")
+            == "Visa Platinum 7000 79** **** 6361"
     )
     assert (
-        mask_account_card("MasterCard 7158300734726758")
-        == "MasterCard 7158 30** **** 6758"
+            mask_account_card("MasterCard 7158300734726758")
+            == "MasterCard 7158 30** **** 6758"
     )
     assert (
-        mask_account_card("Visa Gold 5999414228426353")
-        == "Visa Gold 5999 41** **** 6353"
+            mask_account_card("Visa Gold 5999414228426353")
+            == "Visa Gold 5999 41** **** 6353"
     )
     assert (
-        mask_account_card("Visa Classic 6831982476737658")
-        == "Visa Classic 6831 98** **** 7658"
+            mask_account_card("МИР 6831982476737658")
+            == "МИР 6831 98** **** 7658"
     )
 
 
@@ -29,15 +29,13 @@ def test_mask_account_card():
     "bank_details",
     [
         "Schet 73654108430135874305",
-        "Виза Голд 5999414228426353",
         "Счет 71583007347267585999414228426353",
         "Счет 73654",
         "Счет 0",
         "Счет ",
         "Visa Platinum 70007922896063616831982476737658" "Visa Platinum 70007",
         "Visa Classic ",
-        "73654108430135874305",
-        "",
+        "73654108430135874305"
     ],
 )
 def test_mask_account_card_wrong_number(bank_details):
@@ -48,8 +46,8 @@ def test_mask_account_card_wrong_number(bank_details):
 # Тестирование правильности обработки даты при корректном вводе
 def test_get_date():
     assert (
-        get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
-    )  # Тестирование правильности обработки даты при корректном вводе
+            get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
+    )
 
 
 # Тестирование правильности обработки даты при некорректном вводе
